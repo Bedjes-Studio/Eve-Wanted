0c9910198f1ad58b8a3765e8c57a9e44e52d518f
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-21 23:10:01 +0200

    Add user history (rewards & bounties)

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const { errorHandler } = require("./utils");
+
 const User = require("../models/user");
 
 exports.bestHunters = (req, res, next) => {
@@ -18,6 +20,23 @@ exports.mostWanted = (req, res, next) => {
         .limit(5)
         .then((mostWanted) => {
             res.status(200).json({ mostWanted });
+        })
+        .catch((error) => {
+            errorHandler(error, res);
+        });
+};
+
+/**
+ * Search for all related rewards & bounties
+ */
+
+exports.userHistory = (req, res, next) => {
+    // TODO : Add middleware to detect user
+    Reward.find({ $or: [{ hunter: req.auth.userCharacterId }, { target: req.auth.userCharacterId }] })
+        .then((rewards) => {
+            Bounty.find({ $or: [{ creat: req.auth.userCharacterId }, { target: req.auth.userCharacterId }] }).then((bounties) => {
+                res.status(200).json({ rewards, bounties });
+            });
         })
         .catch((error) => {
             errorHandler(error, res);
