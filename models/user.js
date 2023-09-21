27916e5e9f6faafa64d46dc567eadf13f3cfd68a
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-21 23:05:20 +0200

    Reformat code

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    character_id: { type: Number, required: true },
+    characterId: { type: Number, required: true },
     name: { type: String, required: true },
     birthday: { type: Date, required: true },
-    corporation_id: { type: Number },
+    corporationId: { type: Number },
     token: { type: String },
-    refresh_token: { type: Number },
+    refreshToken: { type: Number },
     hunters: { type: [Number], required: true },
     bounty: { type: Number, required: true },
     maxBounty: { type: Number, required: true },
