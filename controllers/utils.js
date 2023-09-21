7e8d1a99eabce45ae641d4e91f1f3c37d341800e
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-21 23:01:00 +0200

    Add error handler

--- controllers/utils.js
+++ controllers/utils.js
@@ -37,3 +37,8 @@ const apiResponseParser = (response) => {
             });
     });
 };
+
+exports.errorHandler = (error, res) => {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error." });
+};
