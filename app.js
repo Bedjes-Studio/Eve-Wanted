6e085ad18169b610d66ec3cf328cac2ec1653c0d
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-22 12:30:52 +0200

    Setup express & update mongoose

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const apiRoutes = require("./routes/api");
 
 const app = express();
 app.use(express.json());
+app.set("view engine", "ejs");
 
+mongoose.set("strictQuery", true);
 mongoose
     .connect(config.mongodb.host + ":" + config.mongodb.port + "/" + config.mongodb.name, {
         useNewUrlParser: true,
@@ -16,6 +18,7 @@ mongoose
     .then(() => console.log("Connected to MongoDB using port " + config.mongodb.port))
     .catch(() => console.log("Connection to MongoDB failed !"));
 
+app.use("/static", express.static("./static"));
 app.use("/api", apiRoutes);
 app.use("/", frontRoutes);
 
