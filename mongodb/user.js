9bb3c062cbc6118507d5484dcc8f3210d51eabf9
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-24 13:08:21 +0200

    Reformat characterId

--- mongodb/user.js
+++ mongodb/user.js
@@ -1,9 +1,9 @@
 const User = require("../models/user");
 
 const users = [
-    { character_id: "1", name: "User1", birthday: Date.now() },
-    { character_id: "2", name: "User2", birthday: Date.now() },
-    { character_id: "3", name: "User3", birthday: Date.now() },
+    { characterId: "1", name: "User1", birthday: Date.now() },
+    { characterId: "2", name: "User2", birthday: Date.now() },
+    { characterId: "3", name: "User3", birthday: Date.now() },
 ];
 
 exports.userfiller = () => {
