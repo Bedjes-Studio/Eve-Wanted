e8ea4155ebed64ab12a96003d9f5efb2d1584d54
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-24 12:49:23 +0200

    Implements callback code validation

--- controllers/sso.js
+++ controllers/sso.js
@@ -1,6 +1,36 @@
+const { errorHandler, apiCall, apiResponseParser } = require("./utils");
+const config = require("../config");
+
 exports.ssoCallback = (req, res, next) => {
-    console.log("CALLBACK");
-    console.log(req.query.code);
-    console.log(req.query.state);
-    res.status(200).json({ messsage: "callback" });
+    // TODO : validate state
+    getToken(req.query.code)
+        .then((token, refreshToken) => {
+            res.status(200).json({ messsage: "callback", token: token, refreshToken: refreshToken });
+        })
+        .catch((error) => {
+            errorHandler(error, res);
+        });
+
+    // read data from the token
+};
+
+const getToken = (code) => {
+    return new Promise(function (resolve, reject) {
+        let codeValidationEndpoint = "https://login.eveonline.com/v2/oauth/token";
+        let headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Basic " + btoa(config.application.id + ":" + config.application.key),
+        };
+        let body = "grant_type=authorization_code&code=" + code;
+
+        apiCall(codeValidationEndpoint, "POST", headers, body)
+            .then(apiResponseParser)
+            .then((response) => {
+                if (response.statusCode == 200) {
+                    resolve(response.access_token, response.refresh_token);
+                }
+                reject();
+            })
+            .catch(reject);
+    });
 };
