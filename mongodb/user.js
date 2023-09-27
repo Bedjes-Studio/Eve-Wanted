0e5f75daebe6dd47815b3d0eb27bc147f39ffad1
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-09-27 11:42:05 +0200

    Add reward values

--- mongodb/user.js
+++ mongodb/user.js
@@ -1,11 +1,11 @@
 const User = require("../models/user");
 
 const users = [
-    { characterId: "1", name: "User1", birthday: Date.now(), hunters: [], bounty: 10, maxBounty: 0, reward: 0 },
-    { characterId: "2", name: "User2", birthday: Date.now(), hunters: [], bounty: 100, maxBounty: 0, reward: 0 },
-    { characterId: "3", name: "User3", birthday: Date.now(), hunters: [], bounty: 1000, maxBounty: 0, reward: 0 },
-    { characterId: "4", name: "User4", birthday: Date.now(), hunters: [], bounty: 2000, maxBounty: 0, reward: 0 },
-    { characterId: "5", name: "User5", birthday: Date.now(), hunters: [], bounty: 3000, maxBounty: 0, reward: 0 },
+    { characterId: "1", name: "User1", birthday: Date.now(), hunters: [], bounty: 10, maxBounty: 0, reward: 15 },
+    { characterId: "2", name: "User2", birthday: Date.now(), hunters: [], bounty: 100, maxBounty: 0, reward: 5000 },
+    { characterId: "3", name: "User3", birthday: Date.now(), hunters: [], bounty: 1000, maxBounty: 0, reward: 30 },
+    { characterId: "4", name: "User4", birthday: Date.now(), hunters: [], bounty: 2000, maxBounty: 0, reward: 546 },
+    { characterId: "5", name: "User5", birthday: Date.now(), hunters: [], bounty: 3000, maxBounty: 0, reward: 67133156115 },
 ];
 
 exports.userfiller = () => {
